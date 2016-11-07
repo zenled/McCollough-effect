736687fdabd740c1c692fc67c1d9612db1f058ee
Zed <zen.lednik7@gmail.com>
2016-11-07 19:24:15 +0100

    Added general features

--- app/views/main-view/main-view.ts
+++ app/views/main-view/main-view.ts
@@ -1,6 +1,14 @@
-var frameModule = require("ui/frame");
+import frameModule = require("ui/frame");
+let topmost;
 
-exports.tap_Effect = function(){
-  let topmost = frameModule.topmost();
-  topmost.navigate("views/effect-view/effect-view");
+exports.onLoaded = function(args){
+  topmost = frameModule.topmost();
+}
+
+exports.get_effect_tap = function(){
+  topmost.navigate("views/get-effect/get-effect");
+}
+
+exports.about_tap = function(){
+  topmost.navigate("views/about/about");
 }
